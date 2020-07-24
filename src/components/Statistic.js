6952b12feab989dc2f47dd01848c1d3c2bd88e60
Muritala Tobi <muritala.mt@gmail.com>
2020-07-24 05:06:42 +0000

    Initial commit

--- src/components/Statistic.js
+++ src/components/Statistic.js
@@ -91,16 +91,16 @@ export default class Statistic extends Component{
         //   ));
 
         //Comparer Function  
-        // function GetSortOrder(prop) {  
-        //     return function(a, b) {  
-        //         if (a[prop] > b[prop]) {  
-        //             return 1;  
-        //         } else if (a[prop] < b[prop]) {  
-        //             return -1;  
-        //         }  
-        //         return 0;  
-        //     }  
-        // }
+        function GetSortOrder(prop) {  
+            return function(a, b) {  
+                if (a[prop] > b[prop]) {  
+                    return 1;  
+                } else if (a[prop] < b[prop]) {  
+                    return -1;  
+                }  
+                return 0;  
+            }  
+        }
 
         statistics.sort(GetSortOrder("country")); //Pass the attribute to be sorted on  
         // document.write("Sorted Active cases : ");  
